edc2c74d38df38f7be32c88a306165238c2575a1
Roland-Sankara <roland1sankara@gmail.com>
2022-07-01 12:59:02 +0300

    add rest api functionality

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const fs = require('fs')
+const {PrismaClient} = require('@prisma/client');
+const prisma = new PrismaClient();
 
 /*
     // API Endpoints
     
     /quotes => GET, POST
+    /authors => GET, POST
 
     /quotes/:quoteId => GET, PATCH, DELETE
 
@@ -22,64 +24,121 @@ app.use(express.json())
 
 
 // Request Handlers
-app.get('/quotes', (req, res)=>{
-    fs.readFile('quotes.json', (err, quotes)=>{
-        if(quotes){
-            res.send(quotes);
-        }else{
-            res.send('Quotes Not Found');
-        }
-    })
+app.get('/quotes', async(req, res)=>{
+    try {
+        const quotes = await prisma.quote.findMany({})
+        res.status(200).json(quotes);
+    } catch (error) {
+       res.status(500).json('Failed to perform request')
+    }
+    
 })
 
-app.post('/quotes', (req, res)=>{
-    let newQuote = req.body
-    let quotes = [];
-
-    // Get all the quotes
-    fs.readFile('quotes.json', (err,data)=>{
-        if(data){
-            const parsedData = JSON.parse(data);
-            quotes = [...parsedData,newQuote];
-            // Update the quotes.json file
-            fs.writeFile('quotes.json', JSON.stringify(quotes, null, 4), (err)=>{
-                if(err){
-                    res.send('Failed to add quote')
-                }else{
-                    res.send('Successfully added quote')
-                }
-            })
-
-        }else{
-            console.log('File Not Found')
-        }
-    }) 
+app.post('/quotes', async(req, res)=>{
+    try {
+        const newQuote = await prisma.quote.create({
+            data: req.body
+        })
+        res.json({message:'Added quote successfully', quote: newQuote}) 
+    } catch (error) {
+       res.status(400).json('Failed to perform request')
+    }
+    
 })
 
-app.get('/quotes/:quoteId', (req, res)=>{
-    let id = req.params.quoteId
-    let quotes = [];
-    fs.readFile('quotes.json', (err,data)=>{
-        if(data){
-            quotes = JSON.parse(data);
-            let quote = quotes.filter((quote)=>quote.id === id)[0]
-            quote ? res.send(quote) : res.send('Quote not Found')
-        }else{
-            res.send('Quote not found')
-        }
-    })
+app.get('/quotes/:quoteId', async(req, res)=>{
+    try {
+        const quote = await prisma.quote.findUnique({
+            where: {
+                id: Number(req.params.quoteId)
+            },
+            include: {
+                author: true
+            }
+        })
+       res.status(200).json(quote)
+    } catch (error) {
+       res.status(400).json('Failed to perform request')
+    }
+
 })
 
-app.patch('/quotes/:quoteId', (req, res)=>{
-    res.send('Updated the quote')
+app.patch('/quotes/:quoteId', async(req, res)=>{
+    try {
+        const quote = await prisma.quote.update({
+            where: {
+                id: Number(req.params.quoteId)
+            },
+            data: req.body
+        })
+        res.status(200).json({message: 'Successfully Updated Quote', quote})
+    } catch (error) {
+        res.status(400).json('Failed to update quote. Please try again')
+    }
+    
 })
 
-app.delete('/quotes/:quoteId', (req, res)=>{
-    res.send('Deleted the quote')
+app.delete('/quotes/:quoteId', async(req, res)=>{
+    try {
+        const quote = await prisma.quote.delete({
+            where: {
+                id: Number(req.params.quoteId)
+            }
+        })
+        res.status(200).json({message: 'Quote deleted successfully', quote})
+    } catch (error) {
+        res.status(400).json('Failed to update quote. Please try again')
+    }
+})
+// ------------------
+// AUTHORS CRUD 
+// ------------------
+app.get('/authors', async(req, res)=>{
+    try {
+        const authors = await prisma.author.findMany({
+            include: {
+                quotes: true
+            }
+        })
+        res.status(200).json(authors);
+    } catch (error) {
+       res.status(500).json('Failed to perform request')
+    }
+    
+})
+
+app.get('/authors/:authorId', async(req, res)=>{
+    try {
+        const author = await prisma.author.findUnique({
+            where: {
+                id: Number(req.params.authorId)
+            },
+            include: {
+                quotes: true
+            }
+        })
+        res.status(200).json(author);
+    } catch (error) {
+       res.status(500).json('Failed to perform request')
+    }
+    
+})
+
+app.post('/authors', async(req, res)=>{
+    try {
+        const newAuthor = await prisma.author.create({
+            data: req.body
+        })
+        res.status(200).json(newAuthor);
+    } catch (error) {
+       res.status(500).json('Failed to perform request')
+    }
+    
 })
 
 
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is listening on: http://localhost:${PORT}`)
 })
