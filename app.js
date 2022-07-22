c87d6ae9a14e6d10befe2a91785a676766ae5ea5
Roland-Sankara <roland1sankara@gmail.com>
2022-07-22 13:56:52 +0300

    update api endpoint

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.get('/api/v1/', (req, res)=>{
     res.status(200).json({
         status:200, 
-        message:"Awesome the API works 🐻",
+        message:"Awesome the API works fine. Checkout the docs below🐻",
         api_documentation: "https://documenter.getpostman.com/view/6512450/UzJMrv7V" 
     })
 });
